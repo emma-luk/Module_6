367120bcc6e3406c80a39d8b1f53609c91006ba9
emma-luk <emma_luk1@hotmail.com>
2016-06-11 22:59:07 +0100

    commit all again

--- models/truckModel.js
+++ models/truckModel.js
@@ -1,15 +1,16 @@
-// Below you'll find the schema you need to export your truckModel.
-// When exporting the mongoose model you will create using this schema, 
-// name your model 'Truck' (see the example in the slide deck for 'Book')
-// this is to match the name of the collection the data was imported into
+var mongoose = require('mongoose');
 
-// var foodTruckSchema = new Schema({
-//  name: String,
-//  foodType: [String],
-//  schedule: [String],
-//  payment: [String],
-//  description: String,
-//  website: String,
-//  Facebook: String,
-//  Twitter: String
-// });
+var Schema = mongoose.Schema;
+
+var foodTruckSchema = new Schema({
+    name: String,
+    foodType: [String],
+    schedule: [String],
+    payment: [String],
+    description: String,
+    website: String,
+    Facebook: String,
+    Twitter: String
+});
+
+module.exports = mongoose.model('Truck', foodTruckSchema);
